17e90d67de3a3e6964c41d188405d6887a3ce1b3
jatibamoza <jatibamoza@gmail.com>
2025-05-15 18:27:33 +0200

    ajuste por intervalo

--- src/visualizacion.py
+++ src/visualizacion.py
@@ -5,6 +5,9 @@ import pandas as pd
 def visualizar_precio_vs_noticias(df_precios, noticias, nombre_activo, unidad):
     fig = go.Figure()
 
+    # Asegurar que el índice es datetime
+    df_precios.index = pd.to_datetime(df_precios.index)
+
     # Añadir línea de precios
     fig.add_trace(go.Scatter(
         x=df_precios.index,
@@ -23,17 +26,23 @@ def visualizar_precio_vs_noticias(df_precios, noticias, nombre_activo, unidad):
 
     # Añadir puntos de noticias con sentimiento
     for noticia in noticias:
-        fecha = pd.to_datetime(noticia["fecha"])
+        try:
+            fecha = pd.to_datetime(noticia["fecha"])
+        except Exception:
+            continue  # Saltar noticias con fecha inválida
+
         sentimiento = noticia.get("sentimiento", "neutro")
         titulo = noticia.get("titulo", "")
         color = colores.get(sentimiento, "gray")
 
-        # Buscar precio más cercano por fecha
-        if fecha in df_precios.index:
-            precio_cercano = df_precios.loc[fecha]["Close"]
-        else:
-            fecha_mas_cercana = df_precios.index.get_indexer([fecha], method="nearest")[0]
-            precio_cercano = df_precios.iloc[fecha_mas_cercana]["Close"]
+        try:
+            if fecha in df_precios.index:
+                precio_cercano = df_precios.loc[fecha]["Close"]
+            else:
+                fecha_mas_cercana = df_precios.index.get_indexer([fecha], method="nearest")[0]
+                precio_cercano = df_precios.iloc[fecha_mas_cercana]["Close"]
+        except Exception:
+            continue  # Si no se encuentra fecha cercana, saltar
 
         fig.add_trace(go.Scatter(
             x=[fecha],
